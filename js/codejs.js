62c32311dc9a79b5707963fc140d3ef991babf86
Josue Daniel Bustamante <josue1120@outlook.com>
2012-12-17 14:55:15 -0500

    Web-Page casi terminada

--- js/codejs.js
+++ js/codejs.js
@@ -1,22 +1,15 @@
-var x = 1;
-var y = 1;
+$('.toTop').click(function(){
+    $('html,body').animate({scrollTop:0});
+});
 
-if (x == y) {
-    $('.toTop').click(function(){
-        $('html,body').animate({scrollTop:0});
-    });
-    
-    $('.toAbout').click(function(){
-        $('html,body').animate({scrollTop:570});
-    });
-    
-    $('.toServices').click(function(){
-        $('html,body').animate({scrollTop:1520});
-    });
-    
-    $('.toContact').click(function(){
-        $('html,body').animate({scrollTop:2580});
-    });
-} else {
-    
-};
+$('.toAbout').click(function(){
+    $('html,body').animate({scrollTop:570});
+});
+
+$('.toServices').click(function(){
+    $('html,body').animate({scrollTop:1520});
+});
+
+$('.toContact').click(function(){
+    $('html,body').animate({scrollTop:2500});
+});
